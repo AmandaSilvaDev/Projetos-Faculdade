b45c0f79920a27f3e852f8b123298a1fd9d58db9
AmandaSilva <amandasilvalopes.dev@gmail.com>
2023-03-26 13:29:36 -0300

    calcular salario com pesquisa

---
+++ programação-visual/ReajusteDeSalarioComPesquisa/ReajusteDeSalario/ReajusteFolhaDePagamento.Designer.cs
@@ -0,0 +1,215 @@
+﻿namespace ReajusteDeSalario
+{
+    partial class ReajusteFolhaDePagamento
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvDados = new System.Windows.Forms.DataGridView();
+            this.btnArquivo = new System.Windows.Forms.Button();
+            this.txtDiretorio = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.lblPercentualReajuste = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblTotalSemReajuste = new System.Windows.Forms.Label();
+            this.lblTotalComReajuste = new System.Windows.Forms.Label();
+            this.ofdListaFuncionarios = new System.Windows.Forms.OpenFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDados)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dgvDados
+            // 
+            this.dgvDados.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvDados.Location = new System.Drawing.Point(38, 176);
+            this.dgvDados.Margin = new System.Windows.Forms.Padding(4);
+            this.dgvDados.Name = "dgvDados";
+            this.dgvDados.RowHeadersWidth = 51;
+            this.dgvDados.Size = new System.Drawing.Size(726, 269);
+            this.dgvDados.TabIndex = 7;
+            this.dgvDados.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvDados_CellContentClick);
+            // 
+            // btnArquivo
+            // 
+            this.btnArquivo.Location = new System.Drawing.Point(689, 7);
+            this.btnArquivo.Margin = new System.Windows.Forms.Padding(4);
+            this.btnArquivo.Name = "btnArquivo";
+            this.btnArquivo.Size = new System.Drawing.Size(71, 28);
+            this.btnArquivo.TabIndex = 6;
+            this.btnArquivo.Text = "...";
+            this.btnArquivo.UseVisualStyleBackColor = true;
+            this.btnArquivo.Click += new System.EventHandler(this.btnArquivo_Click_1);
+            // 
+            // txtDiretorio
+            // 
+            this.txtDiretorio.Location = new System.Drawing.Point(38, 7);
+            this.txtDiretorio.Margin = new System.Windows.Forms.Padding(4);
+            this.txtDiretorio.Name = "txtDiretorio";
+            this.txtDiretorio.Size = new System.Drawing.Size(643, 22);
+            this.txtDiretorio.TabIndex = 5;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.tableLayoutPanel1);
+            this.panel1.Location = new System.Drawing.Point(38, 52);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(726, 116);
+            this.panel1.TabIndex = 4;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 3;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 211F));
+            this.tableLayoutPanel1.Controls.Add(this.lblPercentualReajuste, 2, 1);
+            this.tableLayoutPanel1.Controls.Add(this.label3, 2, 0);
+            this.tableLayoutPanel1.Controls.Add(this.label2, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.label1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.lblTotalSemReajuste, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.lblTotalComReajuste, 1, 1);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(4, 4);
+            this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(4);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(718, 108);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // lblPercentualReajuste
+            // 
+            this.lblPercentualReajuste.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPercentualReajuste.Location = new System.Drawing.Point(510, 54);
+            this.lblPercentualReajuste.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblPercentualReajuste.Name = "lblPercentualReajuste";
+            this.lblPercentualReajuste.Size = new System.Drawing.Size(203, 39);
+            this.lblPercentualReajuste.TabIndex = 5;
+            this.lblPercentualReajuste.TextAlign = System.Drawing.ContentAlignment.BottomRight;
+            // 
+            // label3
+            // 
+            this.label3.BackColor = System.Drawing.Color.Yellow;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(510, 0);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(203, 23);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Percentual reajuste";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.Color.Yellow;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(257, 0);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(173, 23);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Total com reajuste";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.Yellow;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(4, 0);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(173, 23);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Total sem reajuste";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblTotalSemReajuste
+            // 
+            this.lblTotalSemReajuste.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTotalSemReajuste.Location = new System.Drawing.Point(4, 54);
+            this.lblTotalSemReajuste.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblTotalSemReajuste.Name = "lblTotalSemReajuste";
+            this.lblTotalSemReajuste.Size = new System.Drawing.Size(173, 39);
+            this.lblTotalSemReajuste.TabIndex = 3;
+            this.lblTotalSemReajuste.TextAlign = System.Drawing.ContentAlignment.BottomRight;
+            // 
+            // lblTotalComReajuste
+            // 
+            this.lblTotalComReajuste.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTotalComReajuste.Location = new System.Drawing.Point(257, 54);
+            this.lblTotalComReajuste.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblTotalComReajuste.Name = "lblTotalComReajuste";
+            this.lblTotalComReajuste.Size = new System.Drawing.Size(173, 39);
+            this.lblTotalComReajuste.TabIndex = 4;
+            this.lblTotalComReajuste.TextAlign = System.Drawing.ContentAlignment.BottomRight;
+            // 
+            // ofdListaFuncionarios
+            // 
+            this.ofdListaFuncionarios.FileName = "openFileDialog1";
+            // 
+            // ReajusteFolhaDePagamento
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(817, 525);
+            this.Controls.Add(this.dgvDados);
+            this.Controls.Add(this.btnArquivo);
+            this.Controls.Add(this.txtDiretorio);
+            this.Controls.Add(this.panel1);
+            this.Name = "ReajusteFolhaDePagamento";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.ReajusteFolhaDePagamento_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDados)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvDados;
+        private System.Windows.Forms.Button btnArquivo;
+        private System.Windows.Forms.TextBox txtDiretorio;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Label lblPercentualReajuste;
+        private System.Windows.Forms.Label lblTotalComReajuste;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblTotalSemReajuste;
+        private System.Windows.Forms.OpenFileDialog ofdListaFuncionarios;
+    }
+}
+
---
+++ programação-visual/ReajusteDeSalarioComPesquisa/ReajusteDeSalario/ReajusteFolhaDePagamento.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace ReajusteDeSalario
+{
+    public partial class ReajusteFolhaDePagamento : Form
+
+    {
+        private RepositorioFuncionario repositorio = new RepositorioFuncionario();
+        private BindingSource leituraSource =
+        new BindingSource();
+        public ReajusteFolhaDePagamento()
+        {
+            InitializeComponent();
+            leituraSource.DataSource = repositorio.ObterTodos();
+            dgvDados.DataSource = leituraSource;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnArquivo_Click(object sender, EventArgs e)
+        {
+
+            if (ofdListaFuncionarios.ShowDialog() == DialogResult.OK)
+            {
+                txtDiretorio.Text = ofdListaFuncionarios.FileName;
+                ProcessarArquivo(txtDiretorio.Text);
+                if (repositorio.ObterTodos().Count > 0)
+                {
+                    TotalizarValores(repositorio.ObterTodos());
+                }
+            }
+
+        }
+
+        private void TotalizarValores(IList<Funcionario> dadosLidos)
+        {
+            double totalSemReajuste = 0, totalComReajuste = 0;
+            foreach (var funcionario in dadosLidos)
+            {
+                totalSemReajuste += funcionario.Salario;
+                totalComReajuste += funcionario.
+                NovoSalario;
+            }
+            double percentualReajuste = (totalComReajuste
+            - totalSemReajuste) * 100 / totalSemReajuste;
+            lblTotalSemReajuste.Text = string.Format(
+            "{0:c}", totalSemReajuste);
+            lblTotalComReajuste.Text = string.Format("{0:c}", totalComReajuste);
+            lblPercentualReajuste.Text = string.Format(
+            "{0:n}%", percentualReajuste);
+        }
+        private void ProcessarArquivo(string nomeArquivo)
+        {
+            repositorio.ObterTodos().Clear();
+            string linhaLida;
+            var arquivo = new System.IO.StreamReader(@nomeArquivo);
+            while ((linhaLida = arquivo.ReadLine()) != null)
+            {
+                var dadosLidos = linhaLida.Split(';');
+                var funcionario = new Funcionario
+                {
+                    Codigo = Convert.ToInt32(dadosLidos[0]),
+                    Salario = Convert.ToDouble(dadosLidos[1])
+                };
+                repositorio.Inserir(funcionario);
+            }
+            arquivo.Close();
+        }
+
+
+        private void dgvDados_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void ReajusteFolhaDePagamento_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnArquivo_Click_1(object sender, EventArgs e)
+        {
+
+            if (ofdListaFuncionarios.ShowDialog() == DialogResult.OK)
+            {
+                txtDiretorio.Text = ofdListaFuncionarios.FileName;
+                ProcessarArquivo(txtDiretorio.Text);
+                if (repositorio.ObterTodos().Count > 0)
+                {
+                    TotalizarValores(repositorio.ObterTodos());
+                }
+            }
+        }
+    }
+}
